aa498084baa618d5b73af35b0625fa5c8216a30a
saicastic <saiprithvisuperior@gmail.com>
2025-02-28 23:11:33 +0530

    add layout.js

---
+++ src/app/layout.js
@@ -0,0 +1,19 @@
+import "./globals.css";
+import NavBar from "@/components/common/navbar/Navbar";
+import Footer from "@/components/common/footer/Footer";
+
+export const metadata = {
+  title: "Odyssey",
+  description: "Experience the Excellence !!!",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={` antialiased`}>
+        <NavBar />
+        {children}
+      </body>
+    </html>
+  );
+}
